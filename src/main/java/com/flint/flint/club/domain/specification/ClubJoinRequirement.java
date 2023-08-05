38320851792ab2d063f0cdcdeecc8f53f57cddd5
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2023-08-05 17:13:34 +0900

    Issue : 모임에 필요한 Enum Type 정의 클래스 작성
    1. ClubFrequency.java : 정규모임, 일회성 모임
    2. ClubGenderRequirement.java : 성별 조건
    3. ClubJoinRequirement.java : 모집 조건
    4. ClubMeetingType.java : 온라인, 오프라인 모임

---
+++ src/main/java/com/flint/flint/club/domain/specification/ClubJoinRequirement.java
@@ -0,0 +1,20 @@
+package com.flint.flint.club.domain.specification;
+
+import lombok.Getter;
+
+/**
+ * Club Join Requirement Type Enum Class : Auth or Auto
+ * @author 김기현
+ * @since 2023-08-05
+ */
+@Getter
+public enum ClubJoinRequirement {
+    AUTH_JOIN("Auth"),
+    AUTO_JOIN("Auto");
+
+    private final String type;
+
+    ClubJoinRequirement(String type) {
+        this.type = type;
+    }
+}
