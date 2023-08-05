314f948565bb05c0f59b7fde056da3aff170ef0d
Gihyun Kim <83570546+kimgihean@users.noreply.github.com>
2023-08-05 16:41:28 +0900

    Issue : 모임, 댓글 Repository primary key 타입 UUID로 수정

--- src/main/java/com/flint/flint/club/repository/comment/ClubCommentRepository.java
+++ src/main/java/com/flint/flint/club/repository/comment/ClubCommentRepository.java
@@ -4,6 +4,8 @@ import com.flint.flint.club.domain.comment.ClubComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ClubCommentRepository extends JpaRepository<ClubComment,Long> {
+public interface ClubCommentRepository extends JpaRepository<ClubComment, UUID> {
 }
--- src/main/java/com/flint/flint/club/repository/main/ClubRepository.java
+++ src/main/java/com/flint/flint/club/repository/main/ClubRepository.java
@@ -4,6 +4,8 @@ import com.flint.flint.club.domain.main.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ClubRepository extends JpaRepository<Club, Long> {
+public interface ClubRepository extends JpaRepository<Club, UUID> {
 }
