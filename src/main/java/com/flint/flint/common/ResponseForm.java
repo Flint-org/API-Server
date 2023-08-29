7e69526706ef1e74c4dc57ab50beceb0e4657650
HearHere <jsw5913@naver.com>
2023-08-29 20:03:55 +0900

    refactor @Data로 수정

--- src/main/java/com/flint/flint/common/ResponseForm.java
+++ src/main/java/com/flint/flint/common/ResponseForm.java
@@ -1,7 +1,7 @@
 package com.flint.flint.common;
 
 import com.flint.flint.common.spec.ResultCode;
-import lombok.Getter;
+import lombok.Data;
 
 /**
  * 응답 바디 공통 DTO 양식
@@ -11,7 +11,7 @@ import lombok.Getter;
  * @author 신승건
  * @since 2023-08-08
  */
-@Getter
+@Data
 public class ResponseForm<T> {
 
     private StatusResponse statusResponse = new StatusResponse(ResultCode.OK); // 디폴트로 성공 처리
