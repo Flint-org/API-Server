fa8f53f0247604bc8962a7b0724b934827c7cd49
Gihyun <rlgus2738@naver.com>
2023-09-04 20:01:46 +0900

    infra: 스크립트 파일과 AppSpec 작성

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+REPOSITORY=/home/ubuntu/git-action
+
+echo "> 현재 구동중인 애플리케이션 PID 확인"
+
+CURRENT_PID=$(pgrep -f chord-player)
+
+echo "$CURRENT_PID"
+
+if [ -z $CURRENT_PID ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/build/libs/*.jar $REPOSITORY/
+
+JAR_NAME=$(ls $REPOSITORY/ | grep 'chord-player' | tail -n 1)
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
