479934d6fb7e51cdeb40dcf70abf40a89965ee45
HearHere <jsw5913@naver.com>
2023-09-05 18:59:26 +0900

    remove: 실행 안되는 테스트 삭제

--- src/test/java/com/flint/flint/security/auth/jwt/JwtServiceTest.java
+++ src/test/java/com/flint/flint/security/auth/jwt/JwtServiceTest.java
@@ -1,20 +1,18 @@
 package com.flint.flint.security.auth.jwt;
 
 import com.flint.flint.member.domain.main.Member;
-import com.flint.flint.member.domain.spec.Authority;
-import com.flint.flint.member.domain.spec.Gender;
+import com.flint.flint.member.spec.Authority;
+import com.flint.flint.member.spec.Gender;
 import com.flint.flint.member.repository.MemberRepository;
 import com.flint.flint.security.auth.dto.ClaimsDTO;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class JwtServiceTest {
@@ -26,8 +24,11 @@ class JwtServiceTest {
      JwtService jwtService;
 
     String accessToken;
-    @BeforeAll
-    void setup() {
+    @Test
+    @DisplayName("토큰 검증")
+    @Transactional
+    void verifyToken() {
+
         Member member = Member.builder()
                 .providerId("1234")
                 .authority(Authority.ANAUTHUSER)
@@ -39,11 +40,7 @@ class JwtServiceTest {
         memberRepository.save(member);
         ClaimsDTO claimsDTO = ClaimsDTO.from(member);
         accessToken = jwtService.generateAccessToken(claimsDTO);
-    }
 
-    @Test
-    @DisplayName("토큰 검증")
-    void verifyToken() {
         System.out.println("엑세스 토큰:" + accessToken);
         Date expiration = jwtService.parseExpiration(accessToken);
         boolean isAlreadyExpired = expiration.after(new Date());
@@ -51,5 +48,6 @@ class JwtServiceTest {
         System.out.println("new date:" + new Date());
         System.out.println("isAlreadyExpired:" + isAlreadyExpired);
     }
+
 
 }
