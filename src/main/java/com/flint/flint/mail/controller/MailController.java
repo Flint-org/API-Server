d2f14f5b995f18b464a4874a3d587691b00a2ce0
HearHere <jsw5913@naver.com>
2023-09-05 18:57:54 +0900

    feat: 명함 자동 생성 기능 추가

--- src/main/java/com/flint/flint/mail/controller/MailController.java
+++ src/main/java/com/flint/flint/mail/controller/MailController.java
@@ -5,12 +5,6 @@ import com.flint.flint.mail.dto.request.SendEmailAuthNumberReqeust;
 import com.flint.flint.mail.dto.request.VeriyEmailAuthnumberRequest;
 import com.flint.flint.mail.dto.response.EmailAuthNumberRespose;
 import com.flint.flint.mail.service.MailService;
-import com.flint.flint.mail.service.RateLimitService;
-import com.flint.flint.member.domain.main.Member;
-import com.flint.flint.member.domain.spec.Authority;
-import com.flint.flint.member.service.MemberService;
-import com.flint.flint.redis.RedisUtil;
-import com.flint.flint.security.auth.AuthenticationService;
 import com.flint.flint.security.auth.dto.AuthenticationResponse;
 import com.flint.flint.security.auth.dto.AuthorityMemberDTO;
 import lombok.RequiredArgsConstructor;
@@ -19,9 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static com.flint.flint.common.spec.ResultCode.MAIL_AUTHNUMBER_NOT;
-import static com.flint.flint.common.spec.ResultCode.USER_MANY_REQUEST;
 
 /**
  * 메일 발송 관련 API
@@ -36,6 +27,7 @@ public class MailController {
 
 
     private final MailService mailService;
+
     /**
      * 인증번호 보내기
      * 검증: API호출 횟수
@@ -47,7 +39,7 @@ public class MailController {
     }
 
     /**
-     * 유저 권한 바꿔주고
+     * 유저 권한 바꿔주고, 명함 생성해주고
      * 인증 한 유저 권한으로 새로운 액세스 토큰, 리프레쉬 토큰 발급해주기
      * 검증: 이메일 인증코드
      */
