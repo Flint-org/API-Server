38c6b714bc4b860816a39efd3a480dd7a6ba857e
SeungGun <saverrw2864@naver.com>
2023-09-22 22:07:45 +0900

    feat: 게시판 레포지토리에 이름으로 게시판 조회 메소드 추가

--- src/main/java/com/flint/flint/community/repository/BoardRepository.java
+++ src/main/java/com/flint/flint/community/repository/BoardRepository.java
@@ -5,8 +5,11 @@ import com.flint.flint.community.spec.BoardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
     List<Board> findBoardsByBoardType(BoardType type);
+
+    Optional<Board> findBoardByGeneralBoardName(String name);
 }
