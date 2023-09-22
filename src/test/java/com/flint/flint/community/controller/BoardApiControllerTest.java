144d2ec3dcdb3ec725ab2fc23dd6ef6baab3e705
SeungGun <saverrw2864@naver.com>
2023-09-22 22:17:10 +0900

    test: 게시판 즐겨찾기 등록 api 테스트 코드 작성

---
+++ src/test/java/com/flint/flint/community/controller/BoardApiControllerTest.java
@@ -0,0 +1,57 @@
+package com.flint.flint.community.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.flint.flint.community.domain.board.Board;
+import com.flint.flint.community.repository.BoardRepository;
+import com.flint.flint.community.spec.BoardType;
+import com.flint.flint.custom_member.WithMockCustomMember;
+import com.flint.flint.member.domain.main.Member;
+import com.flint.flint.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+class BoardApiControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private BoardRepository boardRepository;
+    @Autowired
+    private MemberRepository memberRepository;
+    private static final String BASE_URL = "/api/v1/boards";
+
+    @Test
+    @DisplayName("학교 인증을 받은 회원이 게시판 즐겨찾기에 등록하면 성공한다.")
+    @WithMockCustomMember
+    void registerBoardBookmark() throws Exception {
+        Board board = Board.builder()
+                .boardType(BoardType.GENERAL)
+                .generalBoardName("자유게시판")
+                .build();
+
+        Board savedBoard = boardRepository.save(board);
+
+        Member member = Member.builder()
+                .name("테스터")
+                .email("test@test.com")
+                .providerName("kakao")
+                .providerId("kakao test")
+                .build();
+
+        memberRepository.save(member);
+
+        this.mockMvc.perform(post(BASE_URL + "/{boardId}/bookmark", savedBoard.getId()))
+                .andExpect(status().isOk());
+    }
+}
