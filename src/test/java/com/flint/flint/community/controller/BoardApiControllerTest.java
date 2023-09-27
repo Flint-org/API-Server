d6864b19ae144f62eb21809839c19ab8ea09b987
SeungGun <saverrw2864@naver.com>
2023-09-27 19:00:30 +0900

    test: 게시판 즐겨찾기 서비스, api 테스트 코드 작성

--- src/test/java/com/flint/flint/community/controller/BoardApiControllerTest.java
+++ src/test/java/com/flint/flint/community/controller/BoardApiControllerTest.java
@@ -1,7 +1,8 @@
 package com.flint.flint.community.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.flint.flint.community.domain.board.Board;
+import com.flint.flint.community.domain.board.BoardBookmark;
+import com.flint.flint.community.repository.BoardBookmarkRepository;
 import com.flint.flint.community.repository.BoardRepository;
 import com.flint.flint.community.spec.BoardType;
 import com.flint.flint.custom_member.WithMockCustomMember;
@@ -15,7 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -27,6 +28,8 @@ class BoardApiControllerTest {
     private MockMvc mockMvc;
     @Autowired
     private BoardRepository boardRepository;
+    @Autowired
+    private BoardBookmarkRepository boardBookmarkRepository;
     @Autowired
     private MemberRepository memberRepository;
     private static final String BASE_URL = "/api/v1/boards";
@@ -52,6 +55,36 @@ class BoardApiControllerTest {
         memberRepository.save(member);
 
         this.mockMvc.perform(post(BASE_URL + "/{boardId}/bookmark", savedBoard.getId()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("학교 인증을 받은 회원이 즐겨찾기된 게시판을 즐겨찾기 해제하면 성공한다.")
+    @WithMockCustomMember
+    void deleteBoardBookmark() throws Exception {
+        Board board = Board.builder()
+                .boardType(BoardType.GENERAL)
+                .generalBoardName("자유게시판")
+                .build();
+
+        Board savedBoard = boardRepository.save(board);
+
+        Member member = Member.builder()
+                .name("테스터")
+                .email("test@test.com")
+                .providerName("kakao")
+                .providerId("kakao test")
+                .build();
+
+        memberRepository.save(member);
+
+        BoardBookmark bookmark = BoardBookmark.builder()
+                .board(savedBoard)
+                .member(member)
+                .build();
+        boardBookmarkRepository.save(bookmark);
+
+        this.mockMvc.perform(delete(BASE_URL + "/{boardId}/bookmark", savedBoard.getId()))
                 .andExpect(status().isOk());
     }
 }
