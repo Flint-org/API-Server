fcc002f7d02ac454d94d6061dfce090ec121ff58
HearHere <jsw5913@naver.com>
2023-10-10 14:17:54 +0900

    refactor: NPE 관련 코드 수정

---
+++ src/main/java/com/flint/flint/community/dto/response/PostCommentUpdateResponse.java
@@ -0,0 +1,13 @@
+package com.flint.flint.community.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class PostCommentUpdateResponse {
+
+    private long postCommentId;
+}
