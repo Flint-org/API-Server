500bf3d0fdc25fa60e859c54f740f8d7e966ba37
SeungGun <saverrw2864@naver.com>
2023-10-14 14:34:26 +0900

    feat: 즐겨찾기 게시판 목록 조회에 게시판 분류를 기준으로 정렬 기능 추가

--- src/main/java/com/flint/flint/community/repository/BoardBookmarkRepository.java
+++ src/main/java/com/flint/flint/community/repository/BoardBookmarkRepository.java
@@ -4,6 +4,7 @@ import com.flint.flint.community.domain.board.Board;
 import com.flint.flint.community.domain.board.BoardBookmark;
 import com.flint.flint.member.domain.main.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,6 @@ import java.util.Optional;
 public interface BoardBookmarkRepository extends JpaRepository<BoardBookmark, Long> {
     Optional<BoardBookmark> findBoardBookmarkByMemberAndBoard(Member member, Board board);
 
+    @Query("SELECT bm FROM BoardBookmark bm WHERE bm.member = :member ORDER BY bm.board.boardType")
     List<BoardBookmark> findBoardBookmarksByMember(Member member);
 }
