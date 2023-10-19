87b3f2ecfc2d3d00034501fe9d27db7f6443b324
sunwon <jsw5913@naver.com>
2023-10-19 14:57:17 +0900

    feat: 검색어에 해당하는 게시글 가져오기

---
+++ src/main/java/com/flint/flint/community/service/PostGetService.java
@@ -0,0 +1,33 @@
+package com.flint.flint.community.service;
+
+import com.flint.flint.community.domain.post.Post;
+import com.flint.flint.community.dto.response.AllPostGetResponse;
+import com.flint.flint.community.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PostGetService {
+
+    private final PostRepository postRepository;
+
+    public List<AllPostGetResponse> getAllPost(String keyword) {
+        List<Post> postList = postRepository.findByTitleContainingOrContentsContainingOrderByCreatedAtDesc(keyword);
+
+        return postList.stream()
+                .map(post -> new AllPostGetResponse(post.getTitle(), getFirstImageUrl(post)))
+                .toList();
+    }
+
+    // Post에서 첫 번째 이미지 URL을 가져오는 메서드
+    private String getFirstImageUrl(Post post) {
+        if (post.getPostImages() != null && !post.getPostImages().isEmpty()) {
+            return post.getPostImages().get(0).getImgUrl();
+        } else {
+            return ""; //TODO 기본 이미지 URL을 반환
+        }
+    }
+}
