72c336457ce9a2fc8fa1578301f095a814222581
SeungGun <saverrw2864@naver.com>
2023-11-21 14:24:21 +0900

    feat: 상세 게시글 조회 API 구현

--- src/main/java/com/flint/flint/community/controller/PostGetController.java
+++ src/main/java/com/flint/flint/community/controller/PostGetController.java
@@ -1,15 +1,16 @@
 package com.flint.flint.community.controller;
 
 import com.flint.flint.common.ResponseForm;
+import com.flint.flint.community.dto.response.DetailPostResponse;
 import com.flint.flint.community.dto.response.PostResponse;
 import com.flint.flint.community.service.PostGetService;
+import com.flint.flint.security.auth.dto.AuthorityMemberDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -42,5 +43,21 @@ public class PostGetController {
     public ResponseForm<List<PostResponse>> searchInSpecificBoard(@RequestParam String boardName, @RequestParam String keyword,
                                                                   @PageableDefault(size = 10) Pageable pageable) {
         return new ResponseForm<>(postGetService.searchInSpecificBoard(boardName, keyword, pageable));
+    }
+
+    /**
+     * 게시물 상세 조회
+     *
+     * @param memberDTO 조회 유저
+     * @param postId    조회할 게시글 ID
+     * @return 게시글 관련 모든 메타데이터 및 데이터 정보
+     */
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/{postId}")
+    public ResponseForm<DetailPostResponse> getDetailPost(
+            @AuthenticationPrincipal AuthorityMemberDTO memberDTO,
+            @PathVariable("postId") Long postId
+    ) {
+        return new ResponseForm<>(postGetService.getDetailPost(postId));
     }
 }
