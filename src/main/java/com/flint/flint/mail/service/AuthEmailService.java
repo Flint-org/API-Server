4029c6978678539ec8239f835fd9eb109c0de0f7
sunwon <jsw5913@naver.com>
2024-12-16 22:40:02 +0900

    refactor: 이메일 인증 관련 sendAuthCode 메소드 분리

--- src/main/java/com/flint/flint/mail/service/AuthEmailService.java
+++ src/main/java/com/flint/flint/mail/service/AuthEmailService.java
@@ -39,17 +39,7 @@ public class AuthEmailService {
     @Transactional
     public EmailAuthNumberRespose sendCodeEmail(String email, Long key) {
         if (rateLimitService.checkAPICall(key)) { //API호출 횟수 검사
-            authNumber = randomGenerator.makeRandomNumber();
-            String title = "Flint 회원 가입 인증 이메일 입니다."; // 이메일 제목
-            String content =
-                    "홈페이지를 방문해주셔서 감사합니다." +    //html 형식으로 작성
-                            "<br><br>" +
-                            "인증 번호는 " + authNumber + "입니다." +
-                            "<br>" +
-                            "해당 인증번호를 인증번호 확인란에 기입하여 주세요."; //이메일 내용 삽입
-            sendMailService.sendEmail(email, title, content);
-            redisUtil.saveAuthNumber(key, String.valueOf(authNumber), EXPIRATION);  //인증번호 검증을 위해 레디스에 저장
-            return new EmailAuthNumberRespose(authNumber);
+            return sendAuthCode(email, key);
         }
         throw new FlintCustomException(HttpStatus.TOO_MANY_REQUESTS, ResultCode.USER_MANY_REQUEST);
     }
@@ -63,5 +53,17 @@ public class AuthEmailService {
         return authenticationService.generateToken(member); //인증 한 유저권한을 담은 토큰 발급
     }
 
-
+    private EmailAuthNumberRespose sendAuthCode(String  email, Long key) {
+        authNumber = randomGenerator.makeRandomNumber();
+        String title = "Flint 회원 가입 인증 이메일 입니다."; // 이메일 제목
+        String content =
+                "홈페이지를 방문해주셔서 감사합니다." +    //html 형식으로 작성
+                        "<br><br>" +
+                        "인증 번호는 " + authNumber + "입니다." +
+                        "<br>" +
+                        "해당 인증번호를 인증번호 확인란에 기입하여 주세요."; //이메일 내용 삽입
+        sendMailService.sendEmail(email, title, content);
+        redisUtil.saveAuthNumber(key, String.valueOf(authNumber), EXPIRATION);  //인증번호 검증을 위해 레디스에 저장
+        return new EmailAuthNumberRespose(authNumber);
+    }
 }
